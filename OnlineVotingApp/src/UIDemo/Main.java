f7a68f3e512e57a53303da585396c151dc903ae4
Khoa Tap Hat <164383330+Khoataphat@users.noreply.github.com>
2024-03-23 00:55:07 +0700

    Add files via upload

---
+++ OnlineVotingApp/src/UIDemo/Main.java
@@ -0,0 +1,19 @@
+
+package UIDemo;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WelcomeUI welcomeui = new WelcomeUI();
+        welcomeui.setVisible(true);
+    }
+    
+}
